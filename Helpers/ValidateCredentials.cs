b2b674034fcc09c08debabcc8c9d9492fb36a5f7
panckeyk <tinedagum@gmail.com>
2024-12-07 09:33:11 +0800

    Helpers - modified the validate credentials

--- Helpers/ValidateCredentials.cs
+++ Helpers/ValidateCredentials.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Fastfood_Kiosk_v2.Helpers
 {
@@ -21,33 +22,30 @@ namespace Fastfood_Kiosk_v2.Helpers
         public bool validateCredentials(string username, string password, string userRole)
         {
             PasswordHashing passwordHashing = new PasswordHashing();
-
             try
             {
                 using (var connection = _databaseConnection.GetConnection())
                 {
-                    
-                    var selectQuery = "SELECT Password FROM users_table WHERE Username = @username AND user_role = @userRole";
+                    var selectQuery = "SELECT Password FROM users_table WHERE Username = @username AND User_Role = @userRole";
                     string storedPasswordHash = connection.QuerySingleOrDefault<string>(selectQuery, new
                     {
-                        @username = username,
-                        @userRole = userRole
+                        username = username,
+                        userRole = userRole
                     });
 
-                   
                     if (string.IsNullOrEmpty(storedPasswordHash))
                     {
-                        return false;
+                        return false; // User not found or role mismatch
                     }
 
-                    string currentPasswordHash = passwordHashing.hashPassword(password);
-                    return storedPasswordHash == currentPasswordHash;
-
+                    string enteredPasswordHash = passwordHashing.hashPassword(password);
+                    return storedPasswordHash == enteredPasswordHash;
                 }
             }
             catch (Exception ex)
             {
-                throw new Exception("An error has occurred while accessing the database", ex);
+                MessageBox.Show($"An error occurred while accessing the database: {ex.Message}");
+                return false;
             }
         }
 
